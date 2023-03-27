b704e6763b25cbed86f9fb02d9e495f3db2656d3
Joo-ooo <hengjoo7267@gmail.com>
2023-03-27 21:51:33 +0800

    rectified the payment page

--- payment.php
+++ payment.php
@@ -1,10 +1,11 @@
 <?php 
     session_start();
 ?>
-<!DOCTYPE html>
++<!DOCTYPE html>
 <html lang="en">
 
 <head>
+<<<<<<< Updated upstream
    <!-- Bootstrap CSS Codes -->
         <link rel="stylesheet"
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
@@ -44,6 +45,13 @@
         <title>World of Pets</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+=======
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/payment.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+>>>>>>> Stashed changes
 </head>
 
 <body>
@@ -156,5 +164,3 @@
 </body>
 
 </html>
-        
-
