d6fb6160e85be097b68e480a93b3c7184d7a5032
hk20000 <dragonhk2000@gmail.com>
2023-03-27 17:02:51 +0800

    changed replace footer.inc.php content

--- footer.inc.php
+++ footer.inc.php
@@ -1,3 +1,66 @@
-        <footer class="container">
-            <p>Copyright &copy; 2020 World of Pets Pte. Ltd.</p>
-        </footer>
+    <footer class="container">
+
+        <div class="footerLeft">
+            <div class="footerMenu">
+                <h1 class="fMenuTitle">About Us</h1>
+                <ul class="fList">
+                    <li class="fListItem">
+                        <a href="about.php" style="text-decoration: none">Company</a></li>
+                    <li class="fListItem">Contact</li>
+                    <li class="fListItem">Careers</li>
+                    <li class="fListItem">Affiliates</li>
+                    <li class="fListItem">Stores</li>
+                </ul>
+            </div>
+            <div class="footerMenu">
+                <h1 class="fMenuTitle">Useful Links</h1>
+                <ul class="fList">
+                    <li class="fListItem">Support</li>
+                    <li class="fListItem">Refund</li>
+                    <li class="fListItem">FAQ</li>
+                    <li class="fListItem">Feedback</li>
+                    <li class="fListItem">Stories</li>
+                </ul>
+            </div>
+            <div class="footerMenu">
+                <h1 class="fMenuTitle">Products</h1>
+                <ul class="fList">
+                    <li class="fListItem">
+                        <a href="product-shirt.php" style="text-decoration: none">Shirt</a></li>
+                    <li class="fListItem">
+                        <a href="product-shorts.php" style="text-decoration: none">Short</a></li>
+                    <li class="fListItem">
+                        <a href="product-gymgears.php" style="text-decoration: none">Gym Gears</a></li>
+                    <li class="fListItem">
+                        <a href="product-supplements.php" style="text-decoration: none">Supplements</a></li>
+    <!--                    <li class="fListItem">Hippie</li>-->
+                </ul>
+            </div>
+        </div>
+        
+        <div class="footerRight">
+            <div class="footerRightMenu">
+                <h1 class="fMenuTitle">Subscribe to our newsletter</h1>
+                <div class="fMail">
+                    <input type="text" placeholder="your@email.com" class="fInput">
+                    <button class="fButton">Join!</button>
+                </div>
+            </div>
+            <div class="footerRightMenu">
+                <h1 class="fMenuTitle">Follow Us</h1>
+                <div class="fIcons">
+                    <img src="images/facebook.png" alt="" class="fIcon">
+                    <img src="images/twitter.png" alt="" class="fIcon">
+                    <img src="images/instagram.png" alt="" class="fIcon">
+                    <img src="images/whatsapp.png" alt="" class="fIcon">
+                </div>
+            </div>
+            <div class="footerRightMenu">
+                <span class="copyright">@BiggaToday. All rights reserved. 2023.</span>
+            </div>
+        </div>
+
+    <script src="/js/product_main.js"></script>
+
+
+    </footer>
