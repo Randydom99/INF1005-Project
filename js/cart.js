63b400d6e2de867dec92147c4cc2e0b4e1202c09
Joo-ooo <hengjoo7267@gmail.com>
2023-03-28 13:10:40 +0800

    tidied up the js file

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,22 @@
-var removeCartItemButtons = document.getElementsByClassName('button');
-console.log(removeCartItemButtons);
-for (var i = 0; i < removeCartItemButtons.length; i++) {
-    var button = removeCartItemButtons[i];
-    button.addEventListener('click', function(event) {
-        var buttonClicked = event.target;
-        buttonClicked.parentElement.parentElement.remove();
-        updateCartTotal();
-    });
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready(){
+    var removeCartItemButtons = document.getElementsByClassName('button');
+    console.log(removeCartItemButtons);
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i];
+        button.addEventListener('click', removeCartItem);        
+    }
+}
+
+function removeCartItem(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
 }
 
 function updateCartTotal() {
