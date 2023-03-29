579f4ecb86deb567b8e0c202bddc824728c7b2d3
hk20000 <dragonhk2000@gmail.com>
2023-03-29 17:08:15 +0800

    new navbar only for index.php

---
+++ navbar.php
@@ -0,0 +1,52 @@
+<header>
+    <a href="#" class="logo"><img src="images/updatedlogo2.png" alt="logo"/>BIGGA</a>
+    
+    <input type="checkbox" id="menu-bar">
+    <label for="menu-bar">Menu</label>
+    
+    <nav class="navbar" style="background-color: #fff">
+        <ul>
+            <li>
+                <a href="index.php" class="active">Home</a>
+            </li>
+            <li>
+                <a href="" class="active">Workout +</a>
+                <ul>
+                    <li><a href="chest.php">Chest</a></li>
+                    <li><a href="arms.php">Arms</a></li>
+                    <li><a href="abdominal.php">Abdominal</a></li>
+                    <li><a href="back.php">Back</a></li>
+                    <li><a href="leg.php">Legs</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="product_main.php" class="active">Products +</a>
+                <ul>
+                    <li><a href="product-shirt.php">Shirt</a></li>
+                    <li><a href="product-shorts.php">Short</a></li>
+                    <li><a href="product-gymgears.php">Gym Gears</a></li>
+                    <li><a href="product-supplements.php">Supplement</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="" class="active">About Us</a>
+            </li>
+            <li>
+                <a href="" class="active">Contact</a>
+            </li>
+            
+                    
+            <li>
+                <li>
+                    <a href="register.php">
+                        <h2 class="bi bi-person-circle"></h2>
+                    </a>
+                </li>
+            </li> 
+            
+        </ul>
+
+        
+        
+    </nav>
+</header>
