b18b282d44245e4acb11a26a9fa394b7b98ba598
Randydom99 <randycheang23@gmail.com>
2023-03-31 03:07:11 +0800

    linked shirt products to outsourced websites

--- product-shirt.php
+++ product-shirt.php
@@ -54,29 +54,21 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
 
 <body>
     <?php
-    $product_id ="4";
     include "navbar.php";
-    include "get_product.php";
     ?>
     
     <section class="container content-section">
             <h2 class="section-header">SHIRTS</h2>
             <div class="shop-items">
                 <div class="shop-item">
-                    <span class="shop-item-title">Compression Shirt</span>
                     <img class="shop-item-image" src="images/shirt_product1.png" alt="blue shirt">
                     <?php 
+                        $product_id = "4";
+                        include "get_product.php";
                         echo '<h3>'.$product_name.'</h3>';
                         echo '<h3>$'.$product_price.'</h3>';
                         echo '<h3>'.$product_stock.' left</h3>';
-                    ?>                     
-                            <form method="post" action="add-to-cart.php">
-                                <input type="hidden" name="product_id" value="<?php echo $product_id; ?>">
-                                <input type="hidden" name="product_name" value="<?php echo $product_name; ?>">
-                                <input type="hidden" name="product_price" value="<?php echo $product_price; ?>">
-                                <input type="submit" name="add-to-cart" value="Add to Cart">
-                            </form>
-                    <?php
+
                         if ($_SESSION["loggedIn"] == true)
                         {
                             if($product_stock =="0"){
@@ -85,10 +77,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                             echo '</div>';
                         }
                         else{
-                            echo "<form action='cart.php' method='POST'>";
-                            echo "<input type='hidden' name='pid' value='$product_id'>";
-                            echo "<input type='number' name='quantity' min='1' value='1' required class='qtyinput'>";
-                            echo "<button type='submit' name='addToCart' class='btn btn-light bnav-link margin-left:auto'>Add to cart</button>";
+                            echo '<div class="box-container">';
+                            echo '<a href="https://sbd.sg/collections/singlets-t-shirts/products/sbd-t-shirt-black-with-green-2020-endure-range?variant=33107316801618" >Link to product</a>';
+                            echo '</div>';
                             }
                         }
                         else {
@@ -99,31 +90,14 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                     ?>
                 </div>
                 <div class="shop-item">
-                    <span class="shop-item-title">Muscle Fit Shirt</span>
                     <img class="shop-item-image" src="images/shirt_product2.png" alt="black shirt">
                     <?php 
+                        $product_id = "5";
+                        include "get_product.php";
                         echo '<h3>'.$product_name.'</h3>';
-                    ?>
-                    <div class="shop-item-details">                        
-                        <div class="stars">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star-half-alt"></i>
-                            <div class="product-details">
-                            <?php
-                            echo '<div class="price">$'.$product_price.'</div>';
-        //                    echo '<h4>'.$product_desc.'</h4>';
-                            echo '<h4>'.$product_stock.' left</h4>';
-                            echo '<input type="hidden" name="hidden_name" value="$row["product_name"];"/>';
-                            echo '<input type="hidden" name="hidden_price" value="$row["product_price"];"/>';
-                            echo '<input type="hidden" name="hidden_stock" value="$row["product_stock"];"/>';
-                        ?>
-                            </div>
-                        </div>
-                    </div>
-                    <?php
+                        echo '<h3>$'.$product_price.'</h3>';
+                        echo '<h3>'.$product_stock.' left</h3>';
+
                         if ($_SESSION["loggedIn"] == true)
                         {
                             if($product_stock =="0"){
@@ -132,12 +106,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                             echo '</div>';
                         }
                         else{
-                            echo "<form action='cart.php' method='POST'>";
-                            echo '<h4>'.$product_id.'</h4>'; 
-                            echo "<input type='hidden' name='pid' value='$product_id'>";
-                            echo "<input type='number' name='quantity' min='1' value='1' required class='qtyinput'>";
-//                            echo "<button type='submit' name='addToCart' class='btn btn-light bnav-link margin-left:auto'>Add to cart</button>";
-                            echo '<a href="add-to-cart.php?product_id=4">Add to Cart</a>';
+                            echo '<div class="box-container">';
+                            echo '<a href="https://sbd.sg/collections/singlets-t-shirts/products/sbd-t-shirt-2022-phantom-range?variant=40195242688594" >Link to product</a>';
+                            echo '</div>';
                             }
                         }
                         else {
@@ -148,31 +119,14 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                     ?>
                 </div>
                 <div class="shop-item">
-                    <span class="shop-item-title">Phantom Series</span>
                     <img class="shop-item-image" src="images/shirt_product3.png" alt="black-red shirt">
-                    <?php 
-                        echo '<h3>'.$product_name.'</h3>';
-                    ?>
-                    <div class="shop-item-details">                        
-                        <div class="stars">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star-half-alt"></i>
-                            <div class="product-details">
                             <?php
-                            echo '<div class="price">$'.$product_price.'</div>';
-        //                    echo '<h4>'.$product_desc.'</h4>';
-                            echo '<h4>'.$product_stock.' left</h4>';
-                            echo '<input type="hidden" name="hidden_name" value="$row["product_name"];"/>';
-                            echo '<input type="hidden" name="hidden_price" value="$row["product_price"];"/>';
-                            echo '<input type="hidden" name="hidden_stock" value="$row["product_stock"];"/>';
-                        ?>
-                            </div>
-                        </div>
-                    </div>
-                    <?php
+                            $product_id = "6";
+                            include "get_product.php";
+                            echo '<h3>'.$product_name.'</h3>';
+                            echo '<h3>$'.$product_price.'</h3>';
+                            echo '<h3>'.$product_stock.' left</h3>';
+                      
                         if ($_SESSION["loggedIn"] == true)
                         {
                             if($product_stock =="0"){
@@ -181,10 +135,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                             echo '</div>';
                         }
                         else{
-                            echo "<form action='cart.php' method='POST'>";
-                            echo "<input type='hidden' name='pid' value='$product_id'>";
-                            echo "<input type='number' name='quantity' min='1' value='1' required class='qtyinput'>";
-                            echo "<button type='submit' name='addToCart' class='btn btn-light bnav-link margin-left:auto'>Add to cart</button>";
+                            echo '<div class="box-container">';
+                            echo '<a href="https://sbd.sg/collections/singlets-t-shirts/products/classic-t-shirt?variant=39314321702994" >Link to product</a>';
+                            echo '</div>';
                             }
                         }
                         else {
@@ -195,31 +148,14 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                     ?>
                 </div>
                 <div class="shop-item">
-                    <span class="shop-item-title">Tank Top</span>
                     <img class="shop-item-image" src="images/shirt_product4.png" alt="tank top">
                     <?php 
+                        $product_id = "7";
+                        include "get_product.php";
                         echo '<h3>'.$product_name.'</h3>';
-                    ?>
-                    <div class="shop-item-details">                        
-                        <div class="stars">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star-half-alt"></i>
-                            <div class="product-details">
-                            <?php
-                            echo '<div class="price">$'.$product_price.'</div>';
-        //                    echo '<h4>'.$product_desc.'</h4>';
-                            echo '<h4>'.$product_stock.' left</h4>';
-                            echo '<input type="hidden" name="hidden_name" value="$row["product_name"];"/>';
-                            echo '<input type="hidden" name="hidden_price" value="$row["product_price"];"/>';
-                            echo '<input type="hidden" name="hidden_stock" value="$row["product_stock"];"/>';
-                        ?>
-                            </div>
-                        </div>
-                    </div>
-                    <?php
+                        echo '<h3>$'.$product_price.'</h3>';
+                        echo '<h3>'.$product_stock.' left</h3>';
+
                         if ($_SESSION["loggedIn"] == true)
                         {
                             if($product_stock =="0"){
@@ -228,10 +164,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                             echo '</div>';
                         }
                         else{
-                            echo "<form action='cart.php' method='POST'>";
-                            echo "<input type='hidden' name='pid' value='$product_id'>";
-                            echo "<input type='number' name='quantity' min='1' value='1' required class='qtyinput'>";
-                            echo "<button type='submit' name='addToCart' class='btn btn-light bnav-link margin-left:auto'>Add to cart</button>";
+                            echo '<div class="box-container">';
+                            echo '<a href="https://sbd.sg/collections/singlets-t-shirts/products/sbd-tank-2022-defy-range?variant=40017627840594" >Link to product</a>';
+                            echo '</div>';
                             }
                         }
                         else {
@@ -241,7 +176,6 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                         } 
                     ?>
                 </div>
-            </div>
         </section>   
         
         
