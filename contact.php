fc6c534f54ccf7f9c5cf9d997a692815656cfb17
Ming Yi <2201656@sit.singaporetech.edu.sg>
2023-03-31 15:30:43 +0800

    contact form required inputs

--- contact.php
+++ contact.php
@@ -64,22 +64,22 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                 <form action="https://formsubmit.co/biggagymming@gmail.com" method="post" onsubmit="showMessage()">
                     <div class="form-group">
                         <label for="name">Name</label>
-                        <input class="form-control" type="text" name="name">
+                        <input class="form-control" type="text" name="name" required>
                     </div>
 
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input class="form-control" type="email" name="email">
+                        <input class="form-control" type="email" name="email" required>
                     </div>
 
                     <div class="form-group">
                         <label for="subject">Subject</label>
-                        <input class="form-control" type="text" name="subject">
+                        <input class="form-control" type="text" name="subject" required>
                     </div>
 
                     <div class="form-group">
                         <label for="message">Message</label>
-                        <textarea class="form-control" name="message" rows="10"></textarea>
+                        <textarea class="form-control" name="message" rows="10" required></textarea>
                     </div>
 
                     <div class="form-group">
